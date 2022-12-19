22ea96f0611712d32534fb43055ed75263980271
leekwah <kwah.lkh@gmail.com>
2022-12-19 20:44:59 +0900

    ajax

---
+++ photogram/src/main/java/com/cos/photogramstart/web/UserController.java
@@ -0,0 +1,39 @@
+package com.cos.photogramstart.web;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserController {
+    @GetMapping("/user/{id}")
+    public String profile(@PathVariable int id) {
+        return "/user/profile";
+    }
+
+    @GetMapping("/user/{id}/update")
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) { // @@AuthenticationPrincipal 을 통해 Authentication 객체에 바로 접근할 수 있다.
+        // 1번 방법 @AuthenticationPrincipal 를 이용하는 것 - 추천
+        System.out.println("세션 정보 : " + principalDetails.getUser()); // session 내의 user 정보를 얻는다.
+
+        /*
+
+        // 2번 방법 직접 SecurityContextHolder 를 통해서 얻는 것 - 쓰는 것 비추천
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();
+        System.out.println("직접 찾은 세션 정보 : " + mPrincipalDetails.getUser());
+
+
+
+        // 세션 정보를 모델에 넣는 방법 -> 원래는 주석처리한 것을 해야하나, <sec:Authorize> 를 통해서 한다.
+        model.addAttribute("principal", principalDetails.getUser());
+
+        */
+        return "/user/update";
+    }
+}
--- photogram/target/classes/static/js/update.js
+++ photogram/target/classes/static/js/update.js
@@ -1,4 +1,18 @@
 // (1) 회원정보 수정
-function update() {
+function update(userId) {
+    //profileUpdate 는 id 값을 의미한다.
+    // serialize() 를 하면, 모든 값들이 데이터화 된다.
+    let data = $("#profileUpdate").serialize();
 
+    $.ajax({
+        type : "put",
+        url : `/api/user/${userId}`,
+        data : data,
+        contentType : "application/x-www-form-urlencoded; charset=utf-8",
+        dataType : "json"
+    }).done(res => {
+        console.log("update 성공");
+    }).fail(error => {
+        console.log("update 실패");
+    })
 }
