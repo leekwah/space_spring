95ce3ea8e461b43a51e9a6fad3f8322b680cdc2e
leekwah <kwah.lkh@gmail.com>
2023-01-15 17:41:46 +0900

    새로운 프로젝트 기본 구성

---
+++ shop/src/test/java/com/kwah/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwah.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
