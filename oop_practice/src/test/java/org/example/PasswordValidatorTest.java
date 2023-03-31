f8033d50bce2a8de1eca833fab4f9e7332a6452d
leekwah <kwah.lkh@gmail.com>
2023-03-31 12:56:06 +0900

    PasswordValidatorTest2

--- oop_practice/src/test/java/org/example/PasswordValidatorTest.java
+++ oop_practice/src/test/java/org/example/PasswordValidatorTest.java
@@ -18,4 +18,12 @@ public class PasswordValidatorTest {
         assertThatCode(() -> PasswordValidator.validate("leekwahoon"))
                 .doesNotThrowAnyException(); // assertThatCode 안에 있는 부분이 호출되었을 때, 예외가 발생되지 않음을 확인할 때 사용하는 메서드 (테스트 통과)
     }
+
+    @DisplayName("비밀번호가 8자 미만 12자 초과인 경우에는 IllegalArgumentException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest2() {
+        assertThatCode(() -> PasswordValidator.validate("aabb"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 미만이여야 한다.");
+    }
 }
