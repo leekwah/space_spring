5207b26a2faffc0f1d9ae27df567357544cade64
leekwah <kwah.lkh@gmail.com>
2023-04-08 10:51:07 +0900

    음식점에서 음식 주문하는 과정 구현

--- oop_practice/src/test/java/org/example/CustomerTest.java
+++ oop_practice/src/test/java/org/example/CustomerTest.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
 
 /*
  * 음식점에서 음식 주문하는 과정 구현
  */
 public class CustomerTest {
+    @DisplayName("메뉴 이름에 해당하는 요리를 주문한다.")
     @Test
-    void name() {
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈까스", 5000), new MenuItem("냉면", 6000)));
+        Cooking cooking = new Cooking();
 
+        assertThatCode(() -> customer.order("돈까스", menu, cooking))
+                .doesNotThrowAnyException();
     }
 }
