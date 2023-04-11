d48e8aeea69beb21e57eedf3e03f7f280e23f556
leekwah <kwah.lkh@gmail.com>
2023-04-11 20:49:35 +0900

    CustomWAS를 통해서 HTTP Method, Protocol, UrlPath 등을 추출하는 작업

--- was_practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ was_practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -3,10 +3,10 @@ package org.example;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.nio.charset.StandardCharsets;
 
 public class CustomWebApplicationServer {
     private final int port; // port 값
@@ -24,8 +24,23 @@ public class CustomWebApplicationServer {
 
             while ((clientSocket = serverSocket.accept()) != null) { // 클라이언트 소켓이 null이 아니면
                 logger.info("[CustomWebApplicationServer] client connected!"); // 클라이언트가 연결되었다는 의미이다.
-            }
 
+                /*
+                 * Step 1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 */
+
+                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+                    // InputStream을 Reader로 바꾸기 위해서는 BufferedReader가 필요하다.
+                    // 바꾸는 이유는 Line By Line 으로 읽기 위해서이다.
+                    DataOutputStream dos = new DataOutputStream(out);
+
+                    String line;
+                    while((line = br.readLine()) != "") {
+                        System.out.println(line);
+                    }
+                }
+            }
         }
     }
 }
---
+++ was_practice/src/main/java/org/example/QueryStrings.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QueryStrings {
+    private List<QueryString> queryStrings = new ArrayList<>();
+    public QueryStrings(String queryStringLine) {
+        String[] queryStringTokens = queryStringLine.split("&");
+        Arrays.stream(queryStringTokens)
+                .forEach(queryString -> {
+                    String[] values = queryString.split("=");
+                    if(values.length != 2) { // K,V 형태가 아닌 경우
+                        throw new IllegalArgumentException("잘못된 QueryString 포맷을 가진 문자열입니다.");
+                    }
+                    queryStrings.add(new QueryString(values[0], values[1]));
+                });
+    }
+}
