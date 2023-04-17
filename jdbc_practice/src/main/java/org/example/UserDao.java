0d0c5a2999534b86b85555ea2bd2a080524f2996
leekwah <kwah.lkh@gmail.com>
2023-04-17 17:02:04 +0900

    pstmt 분리

--- jdbc_practice/src/main/java/org/example/UserDao.java
+++ jdbc_practice/src/main/java/org/example/UserDao.java
@@ -3,18 +3,6 @@ package org.example;
 import java.sql.*;
 
 public class UserDao {
-    private Connection getConnection() {
-        String url ="jdbc:h2:mem://localhost/~/jdbc_practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
-
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, id, pw);
-        } catch (Exception ex) {
-            return null;
-        }
-    }
     public void create(User user) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -40,6 +28,19 @@ public class UserDao {
         }
     }
 
+    public void create2(User user) throws SQLException {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.executeUpdate(user, sql, new PreparedStatementSetter() {
+            @Override
+            public void setter(PreparedStatement pstmt) throws SQLException {
+                pstmt.setString(1, user.getUserId());
+                pstmt.setString(2, user.getPassword());
+                pstmt.setString(3, user.getName());
+                pstmt.setString(4, user.getEmail());
+            }
+        });
+    }
 
     public User findByUserId(String userId) throws SQLException {
         Connection con = null;
