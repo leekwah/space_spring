4d365e9e00f035c716338b57161419f7c4720934
leekwah <kwah.lkh@gmail.com>
2023-04-18 20:23:01 +0900

    ReflectionAPI 리팩토링

--- mvc_practice/src/test/java/org/example/ReflectionTest.java
+++ mvc_practice/src/test/java/org/example/ReflectionTest.java
@@ -7,7 +7,9 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /*
@@ -18,15 +20,17 @@ public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
     @Test
     void controllerScan() {
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+
+        logger.debug("beans: [{}]", beans);
+    }
+
+    private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
         Reflections reflections = new Reflections("org.example"); // 패키지 찾는 위치
 
         Set<Class<?>> beans = new HashSet<>();
-        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class)); // reflections 해당 패키지에 있는 클래스 내에서 애노테이션이 붙어있는 클래스들을 찾는다는 것을 의미한다.
-        beans.addAll(reflections.getTypesAnnotatedWith(Service.class)); // reflections 해당 패키지에 있는 클래스 내에서 애노테이션이 붙어있는 클래스들을 찾는다는 것을 의미한다.
-        // 이후에 해쉬셋에 더한다.
 
-        // beans에 담겨있다.
-        logger.debug("beans: [{}]", beans);
-
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+        return beans;
     }
 }
