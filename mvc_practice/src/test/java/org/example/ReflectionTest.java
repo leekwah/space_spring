994808664756abf5f997b6b57b0e30cdfbd56bc3
leekwah <kwah.lkh@gmail.com>
2023-04-18 20:18:39 +0900

    ReflectionAPI 기본

--- mvc_practice/src/test/java/org/example/ReflectionTest.java
+++ mvc_practice/src/test/java/org/example/ReflectionTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.annotation.Controller;
+import org.example.annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -21,6 +22,7 @@ public class ReflectionTest {
 
         Set<Class<?>> beans = new HashSet<>();
         beans.addAll(reflections.getTypesAnnotatedWith(Controller.class)); // reflections 해당 패키지에 있는 클래스 내에서 애노테이션이 붙어있는 클래스들을 찾는다는 것을 의미한다.
+        beans.addAll(reflections.getTypesAnnotatedWith(Service.class)); // reflections 해당 패키지에 있는 클래스 내에서 애노테이션이 붙어있는 클래스들을 찾는다는 것을 의미한다.
         // 이후에 해쉬셋에 더한다.
 
         // beans에 담겨있다.
